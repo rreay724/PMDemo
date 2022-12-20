9833c2fda83c7e99eb1016e52206826dfceba642
Bobby Reay <rreay724@gmail.com>
2022-12-20 12:34:44 -0500

    in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import { Navbar } from "./components";
 import { Home, PositionManagement } from "./pages";
+import NestedNavbar from "./pages/nestedNavbar/NestedNavbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/positionManagement" element={<PositionManagement />} />
+        <Route path="/navbar" element={<NestedNavbar />} />
       </Routes>
     </Router>
   );
